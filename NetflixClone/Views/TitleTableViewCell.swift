bb343e2b1a510a495ca7fef16aed10f911ae2f76
Ahmed Halilovic <ahmeddhalilovic@gmail.com>
2023-12-11 16:59:21 +0100

    Coming soon dodao.

---
+++ NetflixClone/Views/TitleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TitleTableViewCell.swift
+//  NetflixClone
+//
+//  Created by Net Solution on 11. 12. 2023..
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
