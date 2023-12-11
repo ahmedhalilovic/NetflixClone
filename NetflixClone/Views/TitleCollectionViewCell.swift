8e92865c676c8982c3842c84bf6b87bae3ca3061
Ahmed Halilovic <ahmeddhalilovic@gmail.com>
2023-12-11 12:02:42 +0100

    Zavrsen Home page sa update ovanim title ima i image u cijeloj tabeli

---
+++ NetflixClone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TitleCollectionViewCell.swift
+//  NetflixClone
+//
+//  Created by Net Solution on 11. 12. 2023..
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    
+}
