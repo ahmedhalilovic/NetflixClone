316434849c5ebc8c9afb1835e05c72c96cfbd4f6
Ahmed Halilovic <ahmeddhalilovic@gmail.com>
2023-12-12 15:26:13 +0100

    Finished Search and Downloads section

--- NetflixClone/Controllers/SearchViewController.swift
+++ NetflixClone/Controllers/SearchViewController.swift
@@ -8,13 +8,81 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    private var titles: [Title] = [Title]()
+    
+    private let discoverTable: UITableView = {
+        
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+
+        view.addSubview(discoverTable)
+        /*
+         Set the delegate and dataSource for the TableView to be that controller, so we can implement all the functions we are going to use to pass the data and pass the number of rows.
+         */
+        discoverTable.delegate = self
+        discoverTable.dataSource = self
+        
+        fetchDiscoverMovies()
     }
     
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        discoverTable.frame = view.bounds
+    }
+    
+    private func fetchDiscoverMovies() {
+        APICaller.shared.getDiscoverMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.discoverTable.reloadData()
+                }
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+}
 
 
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return TitleTableViewCell()
+        }
+        
+        let title = titles[indexPath.row]
+        cell.configure(with: TitleViewModel(titleName: (title.title ?? title.original_title) ?? "Unknown name", posterURL: title.poster_path ?? ""))
+        
+//        let title = titles[indexPath.row]
+//        let model = TitleViewModel(titleName: (title.title ?? title.original_title) ?? "Unknown name", posterURL: title.poster_path ?? "")
+//        cell.configure(with: model)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+    
 }
