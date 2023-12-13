99f3a408996e2a44dd75e066c7a00d778f4ce0cc
Ahmed Halilovic <ahmeddhalilovic@gmail.com>
2023-12-13 13:03:09 +0100

    Finished search tabbar

---
+++ NetflixClone/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultsViewController.swift
+//  NetflixClone
+//
+//  Created by Net Solution on 12. 12. 2023..
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGreen
+    }
+
+}
